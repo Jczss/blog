aacbe720440fdcd8e1f4cda4834cbd5950767edf
jcz <3486969436@qq.com>
2025-01-01 21:16:39 +0800

    网易云嵌入测试

--- utils.jl
+++ utils.jl
@@ -1,8 +1,8 @@
 function hfun_music(vname)
     c = IOBuffer()
-    write(c, "<embed frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" width=280 height=86 src=\"https://music.163.com/outchain/player?type=2&id=")
+    write(c, "<iframe frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" width=\"280\" height=\"86\" src=\"https://music.163.com/outchain/player?type=2&id=")
     write(c, vname[1])
-    write(c, "&auto=0&height=66\"></embed>")
+    write(c, "&auto=0&height=66\"></iframe>")
     return String(take!(c))
 end
 function hfun_bili(vname)
